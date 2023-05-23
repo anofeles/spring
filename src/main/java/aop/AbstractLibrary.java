eeba7112b08cd60bf9ba489e23a50d9736daf8b7
Kakha <kaxam1@gmail.com>
2023-05-23 17:58:39 +0400

    AbstractLibrary abstract getBook

--- src/main/java/aop/AbstractLibrary.java
+++ src/main/java/aop/AbstractLibrary.java
@@ -5,8 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public abstract class AbstractLibrary {
 
-    public void getBook(){
-        System.out.println("add book 2");
-
-    }
+    abstract public void getBook();
 }

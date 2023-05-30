75ea16f1b5cbed31adad962d26776d67832c2b36
Kakha <kaxam1@gmail.com>
2023-05-30 16:17:06 +0400

    add hibernate mysql database connect

--- src/main/java/hibernate/test.java
+++ src/main/java/hibernate/test.java
@@ -1,7 +1,9 @@
 package hibernate;
 
 import hibernate.entiti.Emploi;
-import org.springframework.boot.SpringApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
@@ -11,10 +13,27 @@ import java.util.List;
 public class test {
     public static void main(String[] args) {
         StudentDao studentDao = new StudentDao();
-        Emploi student = new Emploi(2,"tttt1111", "rrrr", "it2","500");
+        Emploi student = new Emploi(5,"tttt1111", "rrrr", "it2","500");
         studentDao.saveStudent(student);
 
         List< Emploi > getStudents = studentDao.getStudents();
         getStudents.forEach(s-> System.out.println(s.getName()));
     }
+
+//    public static void main(String[] args) {
+//        SessionFactory factory =
+//                new Configuration().configure("Hibernate.cfg.xml")
+//                        .addAnnotatedClass(Emploi.class)
+//                        .buildSessionFactory();
+//        try {
+//            Session session = factory.getCurrentSession();
+//            Emploi emploi = new Emploi(5,"kkk","ttttt","it","500");
+//            session.beginTransaction();
+//            session.save(emploi);
+//            session.getTransaction().commit();
+//        }finally {
+//            factory.close();
+//        }
+//
+//    }
 }
